70993cd6586d9f843c3643baddaa99b7329efc38
nolangwoosan <nolangwoosan@gmail.com>
2024-02-22 08:54:33 +0000

    test: add e2e routing test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,18 +1,9 @@
 import { expect, test } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+test("자유게시판 페이지로 이동한다.", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
+  await page.click("text=자유게시판");
+  await expect(page).toHaveURL("http://localhost:3000/board/free");
+  await expect(page.getByText("전체 글")).toBeVisible();
 });
